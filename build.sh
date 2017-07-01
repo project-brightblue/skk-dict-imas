7dd0369591ae02e1d500c8740f4967caa7947208
mlny <me@mlny.info>
2017-07-01 14:09:30 +0900

    Initial commit

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ソート処理
+generate (){
+	
+	# idol name
+	echo 'Generate idols name dict...'
+	cat ./src/FIRSTNAME | skkdic-sort > ./SKK-JISYO.imasml.firstname
+	cat ./src/FULLNAME | skkdic-sort > ./SKK-JISYO.imasml.fullname
+	echo 'done.'
+	
+	# actors
+	echo 'Generate actors dict...'
+	sed -f ./src/ACTORS.sed ./src/ACTORS | iconv -f UTF-8 -t EUC-JP | skkdic-sort > ./SKK-JISYO.imasml.actors.euc
+	cat ./src/ACTORS | skkdic-sort > ./SKK-JISYO.imasml.actors.utf8
+	echo 'done.'
+
+}
+
+which skkdic-sort > /dev/null 2>&1
+
+if [ $? = 0 ]; then
+	generate
+else
+	echo 'Please install skkdic-sort(Included in skktools).'
+	echo 'Ubuntu/Debian: '
+	echo '$ sudo apt install skktools'
+	echo 'Arch Linux: '
+	echo '$ sudo pacman -S skktools'
+	echo 'macOS(Requires Homebrew): '
+	echo '$ brew install skktools'
+fi
+
