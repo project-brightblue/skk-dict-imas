d03052b26f507476ccbc702d6fd44aec2eba11cc
Lon Sagisawa <me@mlny.info>
2018-05-10 22:57:24 +0900

    v180510a
    fix: ATOK辞書をちゃんと使えるように

--- ATOK/build.sh
+++ ATOK/build.sh
@@ -7,8 +7,10 @@ generate (){
     skkdic-expr2 ATOK-DICT.imasml.actors + ATOK-DICT.imasml.firstname + ATOK-DICT.imasml.fullname > ATOK-DICT.imasml.tmp
     rm ATOK-DICT.imasml.actors ATOK-DICT.imasml.firstname ATOK-DICT.imasml.fullname
     # 統合辞書をATOKフォーマットに変換する
-    echo '!!ATOK_TANGO_TEXT_HEADER_1' > ATOK-DICT.imasml.txt
+    echo -e '!!ATOK_TANGO_TEXT_HEADER_1\r\n!!一覧出力\r\n!!対象辞書;\r\n!!単語種類;登録単語(*)\r\n!!出力日時;2018-05-10 00:00:00 +0000\r\n' > ATOK-DICT.imasml.txt
     sed -f skktoatok.sed ATOK-DICT.imasml.tmp >> ATOK-DICT.imasml.txt
+    iconv -f UTF-8 -t UTF-16 ATOK-DICT.imasml.txt -o ATOK-DICT.imasml.txt.utf16
+    mv ATOK-DICT.imasml.txt.utf16 ATOK-DICT.imasml.txt
     rm ATOK-DICT.imasml.tmp
 	echo 'done.'
 }
