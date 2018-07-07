30fc4fe08ccb877c9acc9a1ccba1306b4c951d6b
Lon Sagisawa <me@mlny.info>
2018-07-08 00:02:40 +0900

    libskkの実装でUTF-8辞書を使えるようにする

--- build.sh
+++ build.sh
@@ -10,13 +10,15 @@ generate (){
 	# actors
 	echo 'Generate actors dict...'
 	sed -f ./src/ACTORS.sed ./src/ACTORS | iconv -f UTF-8 -t EUC-JP | skkdic-sort > ./SKK-JISYO.imascg.actors.euc
-	cat ./src/ACTORS | skkdic-sort > ./SKK-JISYO.imascg.actors.utf8
+	echo ';; -*- fundamental -*- ; coding: utf-8 -*-' > ./SKK-JISYO.imascg.actors.utf8
+	cat ./src/ACTORS | skkdic-sort >> ./SKK-JISYO.imascg.actors.utf8
 	echo 'done.'
 
 	# song
 	echo 'Generate song dict...'
 	sed -f ./src/SONG.sed ./src/SONG | iconv -f UTF-8 -t EUC-JP | skkdic-sort > ./SKK-JISYO.imascg.song.euc
-	cat ./src/SONG | skkdic-sort > ./SKK-JISYO.imascg.song.utf8
+	echo ';; -*- fundamental -*- ; coding: utf-8 -*-' > ./SKK-JISYO.imascg.song.utf8
+	cat ./src/SONG | skkdic-sort >> ./SKK-JISYO.imascg.song.utf8
 	echo 'done.'
 
 	# unified
@@ -24,7 +26,8 @@ generate (){
 	skkdic-expr2 SKK-JISYO.imascg.actors.euc + SKK-JISYO.imascg.song.euc + SKK-JISYO.imascg.firstname + SKK-JISYO.imascg.fullname > SKK-JISYO.imascg.unified.euc
 	cat ./SKK-JISYO.imascg.firstname | iconv -f EUC-JP -t UTF-8 > ./firstname.utf8
 	cat ./SKK-JISYO.imascg.fullname | iconv -f EUC-JP -t UTF-8 > ./fullname.utf8
-	skkdic-expr2 SKK-JISYO.imascg.actors.utf8 + SKK-JISYO.imascg.song.utf8 + ./firstname.utf8 + ./fullname.utf8 > SKK-JISYO.imascg.unified.utf8
+	echo ';; -*- fundamental -*- ; coding: utf-8 -*-' > ./SKK-JISYO.imascg.unified.utf8
+	skkdic-expr2 SKK-JISYO.imascg.actors.utf8 + SKK-JISYO.imascg.song.utf8 + ./firstname.utf8 + ./fullname.utf8 >> SKK-JISYO.imascg.unified.utf8
 	rm firstname.utf8 fullname.utf8
 	echo 'done.'
 }
