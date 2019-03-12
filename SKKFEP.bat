da07bec68a9f6c1846f4fa1f6fd96192ef9cd71f
Lon Sagisawa <lon.sagisawa@gmail.com>
2019-03-12 20:50:12 +0900

    SKKFEP導入用スクリプトでunified辞書を使うように

--- SKKFEP.bat
+++ SKKFEP.bat
@@ -9,8 +9,7 @@ echo.
 
 cd /d %~dp0
 
-copy /Y %SRC%\SKK-JISYO.imassc.firstname %DIST%\SKK-JISYO.imassc.firstname > nul 2>&1
-copy /Y %SRC%\SKK-JISYO.imassc.fullname %DIST%\SKK-JISYO.imassc.fullname > nul 2>&1
+copy /Y %SRC%\SKK-JISYO.imassc.unified %DIST%\SKK-JISYO.imassc.unified > nul 2>&1
 
 echo 辞書をコピーしました。
 echo ###########################################
